46f3a94ee27871e232b38b47256e9bd17bcee9d1
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-03-26 09:00:19 -0400

    add messages, complete songs.js file, and start adding to artists.js file

--- api/routes/artists.js
+++ api/routes/artists.js
@@ -6,24 +6,69 @@ const Messages = require("../../messages/messages");
 // TODO change messages for all
 // GET routes
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Artists - GET",
-  });
+  Artist.find({})
+    .select("name album")
+    .exec()
+    .then((artistList) => {
+      if (artistList < 1) {
+        res.status(200).json({
+          message: Messages.artists_empty,
+        });
+      }
+      res.status(200).json({
+        message: Messages.artists,
+        artistList,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
 router.get("/:artistId", (req, res, next) => {
   const artistId = req.params.artistId;
-  res.status(200).json({
-    message: "Artists - GET",
-    id: artistId,
-  });
+
+  Artist.findById({ _id: artistId })
+    .select("name album _id")
+    .populate("song", "title album")
+    .exec()
+    .then((artist) => {
+      if (!artist) {
+        console.log(artist);
+        res.status(404).json({
+          message: Messages.artist_not_found,
+        });
+      }
+      res.status(201).json({
+        artist,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
 // POST route
 router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "Artists - POST",
-  });
+  Artist.find({ name: req.body.name, album: req.body.album })
+    .exec()
+    .then((result) => {
+      if (result.length > 0) {
+        return res.status(409).json({
+          message: Messages.artist_post_duplicate,
+        });
+      }
+
+      const newArtist = new Artist();
+    });
 });
 
 // PATCH route
--- api/routes/songs.js
+++ api/routes/songs.js
@@ -3,16 +3,22 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const Song = require("../models/song");
 const Messages = require("../../messages/messages");
+const { restart } = require("nodemon");
 
-//TODO change messages for all
 // GET routes
 router.get("/", (req, res, next) => {
   Song.find({})
+    .select("title artist album _id")
+    .exec()
     .then((songList) => {
-      // console.log(result);
+      // validation to check if collection is empty
+      if (songList < 1) {
+        res.status(200).json({
+          message: Messages.song_collection_empty,
+        });
+      }
       res.status(200).json({
-        //* message: Messages.[messagesKey]
-        message: "Song Collection",
+        message: Messages.song_collection,
         songList,
       });
     })
@@ -25,50 +31,24 @@ router.get("/", (req, res, next) => {
     });
 });
 
-// TODO add select, populate, exec, and catch methods
 router.get("/:songId", (req, res, next) => {
   const songId = req.params.songId;
 
-  Song.findById({ _id: songId }).then((result) => {
-    console.log(result);
-    res.status(200).json({
-      message: "Song selected",
-      song: {
-        title: result.title,
-        artist: result.artist,
-        album: result.album,
-        id: result._id,
-      },
-    });
-  });
-});
-
-// POST route
-// TODO add validation and methods from GetById
-router.post("/", (req, res, next) => {
-  const newSong = new Song({
-    _id: mongoose.Types.ObjectId(),
-    title: req.body.title,
-    artist: req.body.artist,
-    album: req.body.album,
-  });
-
-  // write new song info to the database
-  newSong
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Song submitted",
-        result,
-        metadata: {
-          method: req.method,
-          host: req.hostname,
-        },
+  Song.findById({ _id: songId })
+    .select("title artist album _id")
+    .exec()
+    .then((song) => {
+      console.log(song);
+      if (!song) {
+        res.status(404).json({
+          message: Messages.song_not_found,
+        });
+      }
+      res.status(200).json({
+        song,
       });
     })
     .catch((err) => {
-      console.error(err.message);
       res.status(500).json({
         error: {
           message: err.message,
@@ -77,9 +57,67 @@ router.post("/", (req, res, next) => {
     });
 });
 
+// POST route
+router.post("/", (req, res, next) => {
+  Song.find({ title: req.body.title, artist: req.body.artist, album: req.body.album })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      // validation to check if song is already in database
+      if (result.length > 0) {
+        return res.status(409).json({
+          message: Messages.song_post_duplicate,
+        });
+      }
+
+      // create song object
+      const newSong = new Song({
+        _id: mongoose.Types.ObjectId(),
+        title: req.body.title,
+        artist: req.body.artist,
+        album: req.body.album,
+      });
+
+      // write new song info to the database
+      newSong
+        .save()
+        .then((song) => {
+          console.log(song);
+          res.status(201).json({
+            message: Messages.song_submitted,
+            song: {
+              title: song.title,
+              artist: song.artist,
+              album: song.album,
+              id: song._id,
+            },
+            metadata: {
+              method: req.method,
+              host: req.hostname,
+            },
+          });
+        })
+        .catch((err) => {
+          console.error(err.message);
+          res.status(500).json({
+            error: {
+              message: err.message,
+            },
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      restart.status(500).json({
+        error: {
+          message: Messages.song_post_error,
+        },
+      });
+    });
+});
+
 // PATCH route
 // TODO add validation and other methods
-// TODO change updateOne to findByIdAndUpdate
 router.patch("/:songId", (req, res, next) => {
   const songId = req.params.songId;
 
@@ -89,12 +127,21 @@ router.patch("/:songId", (req, res, next) => {
     album: req.body.album,
   };
 
-  Song.updateOne({ _id: songId }, { $set: updateSong })
+  Song.findByIdAndUpdate({ _id: songId }, { $set: updateSong })
+    .exec()
     .then((result) => {
+      // validation to check if song is in collection
+      if (!songId) {
+        console.log(result);
+        res.status(404).json({
+          error: {
+            message: Messages.song_not_found,
+          },
+        });
+      }
       res.status(200).json({
-        message: "Song updated",
-        result,
-        song: {
+        message: Messages.song_updated,
+        result: {
           title: updateSong.title,
           artist: updateSong.artist,
           album: updateSong.album,
@@ -126,11 +173,23 @@ router.delete("/:songId", (req, res, next) => {
     album: req.body.album,
   };
   Song.findByIdAndDelete({ _id: songId }, { $set: deleteSong })
+    .exec()
     .then((result) => {
-      console.log(result);
+      if (!songId) {
+        console.log(result);
+        res.status(404).json({
+          error: {
+            message: Messages.song_not_found,
+          },
+        });
+      }
       res.status(200).json({
-        message: "Song deleted",
+        message: Messages.song_deleted,
         result,
+        request: {
+          method: "GET",
+          url: `http://localhost:3000/songs/${songId}`,
+        },
       });
     })
     .catch((err) => {
